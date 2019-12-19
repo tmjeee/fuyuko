66c77711b28e6a2dde756141ee8179ecd2203184
tmjee <tmjee1@gmail.com>
2019-12-20 00:21:13 +1100

    docker

--- db/docker-run.sh
+++ db/docker-run.sh
@@ -3,7 +3,8 @@
 
 docker stop tmjee-fuyuko-db
 docker rm -f tmjee-fuyuko-db
-docker run --name tmjee-fuyuko-db -d tmjee/fuyuko-db
+docker run --name tmjee-fuyuko-db -d --network my-network tmjee/fuyuko-db
+# docker run --name tmjee-fuyuko-db --network my-network tmjee/fuyuko-db
 
 
 
--- fe/docker-run.sh
+++ fe/docker-run.sh
@@ -2,5 +2,5 @@
 
 docker stop tmjee-fuyuko-fe
 docker rm tmjee-fuyuko-fe
-docker rmi -f tmjee-fuyuko-fe
-docker run --name tmjee-fuyuko-fe -d -p 9999:80 tmjee/fuyuko-fe
+docker run --name tmjee-fuyuko-fe -d -p 9999:80 --network my-network tmjee/fuyuko-fe
+# docker run --name tmjee-fuyuko-fe -p 9999:80 --network my-network tmjee/fuyuko-fe
