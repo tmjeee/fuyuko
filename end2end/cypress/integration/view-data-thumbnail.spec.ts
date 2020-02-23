c530d4b72ed2c428faec929267457b6ae4c45b59
tmjee <tmjee1@gmail.com>
2020-02-23 21:00:13 +1100

    fix travis

--- end2end/cypress/integration/view-data-thumbnail.spec.ts
+++ end2end/cypress/integration/view-data-thumbnail.spec.ts
@@ -436,24 +436,149 @@ describe('view-data-thumbnail spec', () => {
     it(`[string attribute] should allow editing of attributes through edit icon`, () => {
 
         const itemName = `Test-Item-${Math.random()}`;
-        const attributeName = `string attribute`;
-        const value = `string-value-${Math.random()}`;
+
+        const string_attributeName = `string attribute`;
+        const string_attributeValue = `string-value-${Math.random()}`;
+        const string_v = string_attributeValue;
+
+        const text_attributeName = `text attribute`;
+        const text_attributeValue = `text value-${Math.random()}`;
+        const text_v = `${text_attributeValue}`;
+
+        const number_attributeName = `number attribute`;
+        const number_attributeValue = (Math.random().toFixed(1));
+        const number_v = `${number_attributeValue}`;
+
+        const date_attributeName = `date attribute`;
+        const date_attributeValue = `0${(Math.random() * 8 + 1).toFixed(0)}-0${(Math.random() * 8 + 1).toFixed(0)}-${2000 + Number((Math.random() * 10).toFixed(0))}`;
+        const date_v = `${date_attributeValue}`;
+
+        const currency_attributeName = `currency attribute`;
+        const currency_attributeValue = ((Math.random() * 10).toFixed(2));
+        const currency_attributeUnit = 'AUD'
+        const currency_v = `$${currency_attributeValue} ${currency_attributeUnit}`;
+
+        const volume_attributeName = `volume attribute`;
+        const volume_attributeValue = ((Math.random() * 10).toFixed(1));
+        const volume_attributeUnit = 'ml';
+        const volume_v = `${volume_attributeValue} ${volume_attributeUnit}`;
+
+        const dimension_attributeName = `dimension attribute`;
+        const dimension_attributeLengthValue = ((Math.random() * 10).toFixed(1));
+        const dimension_attributeWidthValue = ((Math.random() * 10).toFixed(1));
+        const dimension_attributeHeightValue = ((Math.random() * 10).toFixed(1));
+        const dimension_attributeUnit = 'cm';
+        const dimension_v = [
+            `w:${dimension_attributeWidthValue} ${dimension_attributeUnit}`,
+            `h:${dimension_attributeHeightValue} ${dimension_attributeUnit}`,
+            `l:${dimension_attributeLengthValue} ${dimension_attributeUnit}`
+        ];
+
+
+        const area_attributeName = `area attribute`;
+        const area_attributeValue = ((Math.random() * 10).toFixed(1));
+        const area_attributeUnit = 'm2';
+        const area_v = `${area_attributeValue} ${area_attributeUnit}`;
+
+        const length_attributeName = `length attribute`;
+        const length_attributeValue = ((Math.random() * 10).toFixed(1));
+        const length_attributeUnit = 'cm';
+        const length_v = `${length_attributeValue} ${length_attributeUnit}`;
+
+        const width_attributeName = `width attribute`;
+        const width_attributeValue = ((Math.random() * 10).toFixed(1));
+        const width_attributeUnit = 'cm';
+        const width_v = `${width_attributeValue} ${width_attributeUnit}`;
+
+        const height_attributeName = `height attribute`;
+        const height_attributeValue = ((Math.random() * 10).toFixed(1));
+        const height_attributeUnit = 'cm';
+        const height_v = `${height_attributeValue} ${height_attributeUnit}`;
+
+        const select_attributeName = `select attribute`;
+        const select_attributeValue = `key3`
+        const select_v = `value3`;
+
+        const doubleselect_attributeName = `doubleselect attribute`;
+        const doubleselect_attributeValue1 = `key2`
+        const doubleselect_attributeValue2 = `xkey22`
+        const doubleselect_v = `value2 - xvalue22`;
 
         viewDataThumbnailPage
             .clickAddThumbnail(itemName)
             .clickEditThumbnailIcon(itemName)
             .verifyPopupTitle()
-            .editStringAttribute(attributeName, value)
+
+            // string
+            .editStringAttribute(string_attributeName, string_attributeValue)
             .clickCancel()
-            .verifyThumbnailItemHasNoAttributeValue(itemName, attributeName, [value])
+            .verifyThumbnailItemHasNoAttributeValue(itemName, string_attributeName, [string_attributeValue])
 
             .clickEditThumbnailIcon(itemName)
             .verifyPopupTitle()
-            .editStringAttribute(attributeName, value)
+
+
+            // string
+            .editStringAttribute(string_attributeName, string_attributeValue)
             .clickOk()
+
+            // text
+            // number
+            // date
+            // currency
+            // volume
+            // dimension
+            // area
+            // length
+            // width
+            // height
+            // select
+            // doubleselect
+
+
+
             .clickSave()
             .verifySuccessMessageExists()
-            .verifyThumbnailItemHasAttributeValue(itemName, attributeName, [value])
+            .clickItemShowMore(itemName)
+
+            // string
+            .verifyThumbnailItemHasAttributeValue(itemName, string_attributeName, [string_attributeValue])
+
+            // text
+            .verifyThumbnailItemHasAttributeValue(itemName, text_attributeName, [text_attributeValue])
+
+            // number
+            .verifyThumbnailItemHasAttributeValue(itemName, number_attributeName, [number_attributeValue])
+
+            // date
+            .verifyThumbnailItemHasAttributeValue(itemName, date_attributeName, [date_attributeValue])
+
+            // currency
+            .verifyThumbnailItemHasAttributeValue(itemName, currency_attributeName, [currency_attributeValue, currency_attributeUnit])
+
+            // volume
+            .verifyThumbnailItemHasAttributeValue(itemName, volume_attributeName, [volume_attributeValue, volume_attributeUnit])
+
+            // dimension
+            .verifyThumbnailItemHasAttributeValue(itemName, dimension_attributeName, [dimension_attributeLengthValue, dimension_attributeWidthValue, dimension_attributeHeightValue, dimension_attributeUnit])
+
+            // area
+            .verifyThumbnailItemHasAttributeValue(itemName, area_attributeName, [area_attributeValue, area_attributeUnit])
+
+            // length
+            .verifyThumbnailItemHasAttributeValue(itemName, length_attributeName, [length_attributeValue, length_attributeUnit])
+
+            // width
+            .verifyThumbnailItemHasAttributeValue(itemName, width_attributeName, [width_attributeValue, width_attributeUnit])
+
+            // height
+            .verifyThumbnailItemHasAttributeValue(itemName, height_attributeName, [height_attributeValue, height_attributeUnit])
+
+            // select
+            .verifyThumbnailItemHasAttributeValue(itemName, select_attributeName, [select_v])
+
+            // doubleselect
+            .verifyThumbnailItemHasAttributeValue(itemName, doubleselect_attributeName, [doubleselect_v])
         ;
     });
 });
