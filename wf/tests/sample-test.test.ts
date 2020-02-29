8ebdfa470500544b62636969300580bed3848d56
tmjee <tmjee1@gmail.com>
2020-02-29 20:51:43 +1100

    build server clean up remove travis and circle ci

---
+++ wf/tests/sample-test.test.ts
@@ -0,0 +1,12 @@
+
+import {Engine} from '../src/engine-interface'
+
+describe('test', () => {
+    it('test1', () => {
+
+        expect(true).toBe(true);
+    });
+    it('test2', () => {
+
+    });
+});
