f90c9d2b2e796d8067f584facbc3abf6cd89c15e
tkjee <teckkong.jee@gmail.com>
2020-04-14 22:31:05 +1000

    fix compilation error

--- be/src/config/index.ts
+++ be/src/config/index.ts
@@ -10,9 +10,9 @@ const overrideWithProcessArgv = (config: any) => {
         if (match && match.length == 3) {
             try {
                 config[match[1]] = JSON.parse(match[2]);
-            } catch (e) {
+            } catch (err) {
                 config[match[1]] = (match[2]);
-                e(`failed to parse value ${match[2]} for overriding property ${match[1]} using value as is`, e);
+                e(`failed to parse value ${match[2]} for overriding property ${match[1]} using value as is`, err);
             }
             i(`Command line arguments ${match[1]}=${match[2]} will override the one in config.json`);
         }
