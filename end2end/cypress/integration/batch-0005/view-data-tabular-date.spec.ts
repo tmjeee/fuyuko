43b7cfdeb790d6b5579d8dae49d9c9781efe7792
tkjee <teckkong.jee@gmail.com>
2020-05-12 23:28:27 +1000

    #74 reorder cypress tests

---
+++ end2end/cypress/integration/batch-0005/view-data-tabular-date.spec.ts
@@ -0,0 +1,104 @@
+import {ViewDataTablePage} from "../page-object/sub-page-object/view-data-table.page";
+import {LoginPage} from "../page-object/login.page";
+
+describe('view-data-tabular-date spec', () => {
+    const attrs = [
+        'string attribute',
+        'text attribute',
+        'number attribute',
+        'date attribute',
+        'currency attribute',
+        'volume attribute',
+        'dimension attribute',
+        'area attribute',
+        'length attribute',
+        'width attribute',
+        'height attribute',
+        'select attribute',
+        'doubleselect attribute',
+    ];
+
+
+    let viewDataTablePage: ViewDataTablePage;
+
+    beforeEach(() => {
+        // cy.restoreLocalStorage();
+        const username = Cypress.env('username');
+        const password = Cypress.env('password');
+        viewDataTablePage = new LoginPage()
+            .visit()
+            .login(username, password)
+            .visitViewPage()
+            .visitViewDataTable()
+            .selectGlobalView('Test View 1');
+        // .clickReload()
+        ;
+    });
+
+    afterEach(() => {
+        // cy.saveLocalStorage();
+    });
+
+
+    it(`[date attribute] add item, edit attribute and delete item`, () => {
+        const itemName = `Add-item-${Math.random()}`;
+
+        // text attribute
+        {
+            const attributeName = `date attribute`;
+            const attributeValue = `0${(Math.random() * 8 + 1).toFixed(0)}-0${(Math.random() * 8 + 1).toFixed(0)}-${2000 + Number((Math.random() * 10).toFixed(0))}`;
+            const v = `${attributeValue}`;
+
+            viewDataTablePage
+                .clickReload()
+                // create new item
+                .verifySaveEnable(false)
+                .clickOnAddItem(itemName)
+                .verifyDataTableHasItem(itemName, true)
+                .clickOnItemAttributeCellToEdit(itemName, attributeName)
+                .verifyPopupTitle()
+                .editDateValue(attributeValue)
+                .clickDone()
+
+                // save
+                .verifySaveEnable(true)
+                .verifyAttributeCellExists(attributeName, true)
+                .verifyAttributeCellValue(itemName, attributeName, v)
+                .clickOnSaveItem()
+                .verifySuccessMessageExists()
+                .verifySaveEnable(false)
+                .verifyDataTableHasItem(itemName, true)
+
+                // delete created item
+                .clickOnDeleteItem([itemName])
+                .verifySaveEnable(true)
+                .clickOnSaveItem()
+                .verifySuccessMessageExists()
+                .verifyDataTableHasItem(itemName, false)
+            ;
+        }
+    });
+
+
+
+    it ('[date attribute] edit and cancel should not be saveable', () => {
+        const itemName = `Item-1`;
+
+        // date attribute
+        {
+            const attributeName = `date attribute`;
+            const attributeValue = `0${(Math.random() * 8 + 1).toFixed(0)}-0${(Math.random() * 8 + 1).toFixed(0)}-${2000 + Number((Math.random() * 10).toFixed(0))}`;
+            const v = `${attributeValue}`;
+            viewDataTablePage
+                .clickReload()
+                .clickOnItemAttributeCellToEdit(itemName, attributeName)
+                .verifyPopupTitle()
+                .editDateValue(attributeValue)
+                .clickCancel()
+                .verifyAttributeCellExists(attributeName, true)
+                .verifyAttributeCellNotValue(itemName, attributeName, v)
+                .verifySaveEnable(false)
+            ;
+        }
+    });
+});
---
+++ end2end/cypress/integration/batch-0007/view-data-tabular-select.spec.ts
@@ -0,0 +1,108 @@
+import {ViewDataTablePage} from "../page-object/sub-page-object/view-data-table.page";
+import {LoginPage} from "../page-object/login.page";
+
+describe('view-data-tabular-select spec', () => {
+    const attrs = [
+        'string attribute',
+        'text attribute',
+        'number attribute',
+        'date attribute',
+        'currency attribute',
+        'volume attribute',
+        'dimension attribute',
+        'area attribute',
+        'length attribute',
+        'width attribute',
+        'height attribute',
+        'select attribute',
+        'doubleselect attribute',
+    ];
+
+
+    let viewDataTablePage: ViewDataTablePage;
+
+    beforeEach(() => {
+        // cy.restoreLocalStorage();
+        const username = Cypress.env('username');
+        const password = Cypress.env('password');
+        viewDataTablePage = new LoginPage()
+            .visit()
+            .login(username, password)
+            .visitViewPage()
+            .visitViewDataTable()
+            .selectGlobalView('Test View 1');
+        // .clickReload()
+        ;
+    });
+
+    afterEach(() => {
+        // cy.saveLocalStorage();
+    });
+
+
+
+    it(`[select attribute] add item, edit attribute and delete item`, () => {
+        const itemName = `Add-item-${Math.random()}`;
+
+        // select attribute
+        {
+            const attributeName = `select attribute`;
+            const attributeValue = `key3`
+            const v = `value3`;
+
+            viewDataTablePage
+                .clickReload()
+                // create new item
+                .verifySaveEnable(false)
+                .clickOnAddItem(itemName)
+                .verifyDataTableHasItem(itemName, true)
+                .clickOnItemAttributeCellToEdit(itemName, attributeName)
+                .verifyPopupTitle()
+                .editSelectValue(attributeValue)
+                .clickDone()
+
+                // save
+                .verifySaveEnable(true)
+                .verifyAttributeCellExists(attributeName, true)
+                .verifyAttributeCellValue(itemName, attributeName, v)
+                .clickOnSaveItem()
+                .verifySuccessMessageExists()
+                .verifySaveEnable(false)
+                .verifyDataTableHasItem(itemName, true)
+
+                // delete created item
+                .clickOnDeleteItem([itemName])
+                .verifySaveEnable(true)
+                .clickOnSaveItem()
+                .verifySuccessMessageExists()
+                .verifyDataTableHasItem(itemName, false)
+            ;
+        }
+    });
+
+
+
+
+
+    it ('[select attribute] edit and cancel should not be saveable', () => {
+        const itemName = `Item-1`;
+
+        // select attribute
+        {
+            const attributeName = `select attribute`;
+            const attributeValue = `key3`
+            const v = `value3`;
+            viewDataTablePage
+                .clickReload()
+                .clickOnItemAttributeCellToEdit(itemName, attributeName)
+                .verifyPopupTitle()
+                .editSelectValue(attributeValue)
+                .clickCancel()
+                .verifyAttributeCellExists(attributeName, true)
+                .verifyAttributeCellNotValue(itemName, attributeName, v)
+                .verifySaveEnable(false)
+            ;
+        }
+    });
+
+});
