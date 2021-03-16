e004fd52358a05a3dc325b3a642915fb20ba20a4
tmjeee <tmjeee@gmail.com>
2021-03-16 22:59:04 +1100

    #144 allow git version to be available in config.json (both fe and be)

---
+++ be/update-files-with-git-hash.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cat > githash.js <<EOF
+const fs = require('fs');
+const git_hash = () => {
+    const rev = fs.readFileSync('../.git/HEAD').toString().trim().split(/.*[: ]/).slice(-1)[0];
+    if (rev.indexOf('/') === -1) {
+        return rev;
+    } else {
+        return fs.readFileSync('../.git/' + rev).toString().trim();
+    }
+}
+const replace = async (hash) => {
+  await require('replace-in-file')({
+    files: ['./dist/be/src/config/config.json'],
+    from: '__GIT_VERSION__',
+    to: hash
+  });
+}
+const hash = git_hash();
+replace(hash);
+console.log(hash);
+
+EOF
+
+node githash.js
