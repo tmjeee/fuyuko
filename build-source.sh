5fa9a21ebe660902f5e5e1d9df71e317fc43ca3c
tmjee <tmjee1@gmail.com>
2021-10-13 19:58:00 +1100

    github workflow action fe

--- build-source.sh
+++ build-source.sh
@@ -19,9 +19,11 @@ echo "BUILD BE"
 cd be
 npm install
 npm install 'replace-in-file'
-chmod +x ./update-files-with-git-hash.sh
-./update-files-with-git-hash.sh
 npm run build
+chmod +x ./update-files-with-git-hash.sh
+chmod +x ./update-files-with-git-tag.sh
+./update-files-with-git-hash.sh
+./update-files-with-git-tag.sh
 )
 
 
@@ -30,9 +32,11 @@ echo "BUILD FE"
 cd fe
 npm install
 npm install 'replace-in-file'
-chmod +x ./update-files-with-git-hash.sh
-./update-files-with-git-hash.sh
 npm run build-prod
+chmod +x ./update-files-with-git-hash.sh
+chmod +x ./update-files-with-git-tag.sh
+./update-files-with-git-hash.sh
+./update-files-with-git-tag.sh
 )
 
 echo "DONE BUILD SOURCE"
